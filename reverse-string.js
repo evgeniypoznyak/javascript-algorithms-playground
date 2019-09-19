6dce178a4609f870143e5cb3a91a0747934d6fbd
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-09-19 14:23:20 -0500

    more code

--- reverse-string.js
+++ reverse-string.js
@@ -1,4 +1,6 @@
 // Lecture 66
-const reverseString = str => str.split('').reverse().join('');
+const reverseString1 = str => str.split('').reverse().join('');
+const reverseString2 = str => [...str].reverse().join('');
 
-console.log(reverseString('Hello my name is Evgeniy'));
+console.log(reverseString1('Hello my name is Evgeniy'));
+console.log(reverseString2('Hello my name is Evgeniy'));
