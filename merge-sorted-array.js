3192359013b3ee4480b55e5edf141bd246e8276f
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-09-19 14:21:54 -0500

    Initial commit

---
+++ merge-sorted-array.js
@@ -0,0 +1,15 @@
+const isAllArraysValid = (arr1, arr2) =>
+    Array.isArray(arr1) && Array.isArray(arr2) && arr1.length > 0 && arr2.length > 0;
+
+const mergeSortedArrays = (arr1, arr2) => {
+    if (isAllArraysValid(arr1, arr2)) {
+
+
+
+    }
+};
+
+const arr1 = [1, 2, 3, 43];
+const arr2 = [3, 6, 32, 40];
+
+console.log(mergeSortedArrays(arr1, arr2));
---
+++ reverse-string.js
@@ -0,0 +1,4 @@
+// Lecture 66
+const reverseString = str => str.split('').reverse().join('');
+
+console.log(reverseString('Hello my name is Evgeniy'));
