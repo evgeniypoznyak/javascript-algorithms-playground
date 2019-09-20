cdfb51b936bcb1adcde7042186c6191ab5ea152e
Evgeniy Poznyak <evgeniy.poznyak@coxautoinc.com>
2019-09-20 16:45:12 -0500

    Contains Duplicate. In progress...

---
+++ src/contains-duplicate.js
@@ -0,0 +1,27 @@
+/*
+217. Contains Duplicate
+https://leetcode.com/problems/contains-duplicate/description/
+
+Input: [1,2,3,1]
+Output: true
+
+Input: [1,2,3,4]
+Output: false
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ */
+
+
+const containsDuplicate = nums => {
+    console.log(nums);
+    // todo
+};
+
+const arr1 = [1, 2, 3, 1]; // true
+const arr2 = [1, 2, 3, 4]; // false
+const arr3 = [1, 2, 3, 1]; // true
+
+containsDuplicate(arr1);
+containsDuplicate(arr2);
+containsDuplicate(arr3);
