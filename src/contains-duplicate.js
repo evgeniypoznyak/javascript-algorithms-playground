2479ec3446805d0e6b51163dbc457b23075dfd3e
Evgeniy Poznyak <evgeniy.poznyak@coxautoinc.com>
2019-09-20 17:01:26 -0500

    Contains Duplicate. In progress...

--- src/contains-duplicate.js
+++ src/contains-duplicate.js
@@ -12,16 +12,34 @@ Input: [1,1,1,3,3,4,3,2,4,2]
 Output: true
  */
 
+const arr4 = require('./example1');
 
 const containsDuplicate = nums => {
-    console.log(nums);
-    // todo
+    for (let i = 0; i < nums.length; i++) {
+        let duplicate = 0;
+        nums.map(e => {
+            if (e === nums[i]) {
+                duplicate++;
+            }
+        });
+        if (duplicate < 2) {
+            duplicate = 0;
+        } else {
+            return true;
+        }
+    }
+    return false;
 };
 
 const arr1 = [1, 2, 3, 1]; // true
 const arr2 = [1, 2, 3, 4]; // false
 const arr3 = [1, 2, 3, 1]; // true
 
-containsDuplicate(arr1);
-containsDuplicate(arr2);
-containsDuplicate(arr3);
+
+console.log(containsDuplicate(arr1));
+console.log(containsDuplicate(arr2));
+console.log(containsDuplicate(arr3));
+console.log(arr4.length);
+// console.log(containsDuplicate(arr4));
+
+
