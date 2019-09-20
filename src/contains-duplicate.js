01f105bad2e658035a1ae3b74cbd252fa2c71a77
Evgeniy Poznyak <evgeniy.poznyak@coxautoinc.com>
2019-09-20 17:02:37 -0500

    Contains Duplicate. Description

--- src/contains-duplicate.js
+++ src/contains-duplicate.js
@@ -2,6 +2,10 @@
 217. Contains Duplicate
 https://leetcode.com/problems/contains-duplicate/description/
 
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct.
+
 Input: [1,2,3,1]
 Output: true
 
