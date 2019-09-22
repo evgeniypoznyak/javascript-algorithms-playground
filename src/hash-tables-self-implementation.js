4cfae1e97cf3cb0aabd6187d5c8488ef628ea325
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-09-22 00:54:36 -0500

    hash example

---
+++ src/hash-tables-self-implementation.js
@@ -0,0 +1,48 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+        // this.data = [];
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            const currentHash = hash;
+            const charCodeAtI = key.charCodeAt(i);
+            const len = this.data.length;
+            const complex = currentHash + charCodeAtI * i;
+            const result = complex % len;
+            hash = result;
+            // hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        const address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        const address = this._hash(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.get('grapes');
+myHashTable.set('apples', 9);
+myHashTable.get('apples');
