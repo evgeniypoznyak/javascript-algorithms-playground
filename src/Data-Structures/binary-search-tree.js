598c40e46e12c248da969feaf19d6145983417c1
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-10-20 09:30:24 -0500

    Breadth First Search recursion approach

--- src/Data-Structures/binary-search-tree.js
+++ src/Data-Structures/binary-search-tree.js
@@ -150,6 +150,23 @@ class BinarySearchTree {
         }
         return list;
     }
+
+    breadthFirstSearchR(queue, list) {
+        if (!queue.length) {
+            return list;
+        }
+        const currentNode = queue.shift();
+        list.push(currentNode.value);
+
+        if (currentNode.left) {
+            queue.push(currentNode.left);
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right);
+        }
+
+        return this.breadthFirstSearchR(queue, list);
+    }
 }
 
 const tree = new BinarySearchTree();
@@ -162,6 +179,7 @@ tree.insert(15);
 tree.insert(1);
 
 console.log('BFS', tree.breadthFirstSearch());
+console.log('BFSR', tree.breadthFirstSearchR([tree.root], []));
 
 //     9
 //  4     20
