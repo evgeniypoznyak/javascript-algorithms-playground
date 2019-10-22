b64bab740f21aa2cddbb3e050e0ebd434486c0d4
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-10-22 17:02:46 -0500

    figure out algorithms.

--- src/leetcode/dynamic-programming/house-robber.js
+++ src/leetcode/dynamic-programming/house-robber.js
@@ -21,6 +21,22 @@ Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 Total amount you can rob = 2 + 9 + 1 = 12.
 
+
+return nums.reduce(function(p, n) {
+    return [p[1], Math.max(p[0] + n, p[1])];
+}, [0,0])[1];
+
+
+var rob = function(nums) {
+    if(nums.length === 0) return 0;
+    if(nums.length === 1) return nums[0];
+    let totals = [nums[0], Math.max(nums[0], nums[1])];
+    for(let i = 2; i < nums.length; i ++){
+        totals[i] = Math.max(totals[i - 1], totals[i - 2] + nums[i]);
+    }
+    return totals[totals.length - 1];
+};
+
  */
 
 
@@ -29,5 +45,47 @@ Total amount you can rob = 2 + 9 + 1 = 12.
  * @return {number}
  */
 const rob = nums => {
+    const result = {};
 
+    for (let i = 0; i < nums.length; i++) {
+
+    }
+    return result;
+};
+
+
+console.log(rob([1, 2])); // 2
+console.log(rob([1, 1])); // 1
+console.log(rob([1, 2, 3, 1])); // 1
+console.log(rob([2, 7, 9, 3, 1])); // 1
+
+
+const rob2 = nums => {
+/*
+Runtime: 56 ms, faster than 100.00% of JavaScript online submissions for House Robber.
+Memory Usage: 33.8 MB, less than 25.89% of JavaScript online submissions for House Robber.
+O(n) time, O(1) space
+
+- Bottom up strategy
+- Iterative
+- Memoization
+
+Trick: At index [i], you only need to know the maximum profit at [i - 1] and [i - 2].
+This is a slight variation on fibonacci.
+*/
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < nums.length; i++) {
+        const maxAtCurrent = Math.max(nums[i] + maxAtTwoBefore, maxAtOneBefore);
+
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
+    }
+
+    return maxAtOneBefore;
 };
