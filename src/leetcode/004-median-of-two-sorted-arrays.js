f18c08ffdfaf8578065cf6a7a132dbfe500243f1
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-11-11 13:03:09 -0600

    4. Median of Two Sorted Arrays in progress

--- src/leetcode/004-median-of-two-sorted-arrays.js
+++ src/leetcode/004-median-of-two-sorted-arrays.js
@@ -22,13 +22,37 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
  */
 
+const isEven = n => n % 2 === 0;
+
+const isOdd = n => Math.abs(n % 2) === 1;
+
+const findMedianFromOneArray = arr => {
+    if (isOdd(arr.length)) {
+        const medianIndex = Math.floor((arr.length) / 2);
+        return arr[medianIndex];
+    }
+    if (isEven(arr.length)) {
+        const medianIndex = Math.floor((arr.length) / 2);
+        const diff = (arr[medianIndex] - arr[medianIndex - 1]) / 2;
+        return arr[medianIndex - 1] + diff;
+    }
+};
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 const findMedianSortedArrays = (nums1, nums2) => {
-    return 0;
+    if (nums1.length === 0 && nums2.length === 0) {
+        return 0;
+    }
+    if (nums1.length === 0) {
+       return findMedianFromOneArray(nums2);
+    }
+    if (nums2.length === 0) {
+       return findMedianFromOneArray(nums1);
+    }
 };
 
 
