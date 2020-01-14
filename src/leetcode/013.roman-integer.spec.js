2c0add7bfe2baf6e135114b09a728900e7fb6263
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-14 09:20:53 -0600

    13. Roman to Integer: in progress...

---
+++ src/leetcode/013.roman-integer.spec.js
@@ -0,0 +1,27 @@
+const {solution} = require('./013.roman-integer');
+
+describe('Roman to Integer', () => {
+    it('solution is a function', () => {
+        expect(typeof solution).toEqual('function');
+    });
+
+    it('Should "III" solution return 3', () => {
+        expect(solution('III')).toBe(3);
+    });
+
+    it('Should "IV" solution return 4', () => {
+        expect(solution('IV')).toBe(4);
+    });
+
+    it('Should "IX" solution return 9', () => {
+        expect(solution('IX')).toBe(9);
+    });
+
+    it('Should "LVIII" solution return 9', () => {
+        expect(solution('LVIII')).toBe(58);
+    });
+
+    it('Should "MCMXCIV" solution return 1994', () => {
+        expect(solution('MCMXCIV')).toBe(1994);
+    });
+});
