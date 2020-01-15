baef9c1395337068d15faca52a15e6eb22da3e3e
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-15 07:28:32 -0600

    20. Valid Parentheses: setup

---
+++ src/leetcode/020-valid-parentheses.spec.js
@@ -0,0 +1,27 @@
+const {solution} = require('./020-valid-parentheses');
+
+describe('Longest Common Prefix', () => {
+    it('solution is a function', () => {
+        expect(typeof solution).toEqual('function');
+    });
+
+    it('Should be true if "()" passed in', () => {
+        expect(solution('()')).toBeTruthy();
+    });
+
+    it('Should be true if "()[]{}" passed in', () => {
+        expect(solution('()[]{}')).toBeTruthy();
+    });
+
+    it('Should be true if "{[]}" passed in', () => {
+        expect(solution('{[]}')).toBeTruthy();
+    });
+
+    it('Should be false if "(]" passed in', () => {
+        expect(solution('(]')).toBeFalsy();
+    });
+
+    it('Should be false if "([)]" passed in', () => {
+        expect(solution('([)]')).toBeFalsy();
+    });
+});
