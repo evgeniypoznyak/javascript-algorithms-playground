dfd2c76ffd6d95854ff71573709505d361781260
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-15 07:13:52 -0600

    14. Longest Common Prefix

--- src/leetcode/014-longest-common-prefix.js
+++ src/leetcode/014-longest-common-prefix.js
@@ -22,10 +22,20 @@ All given inputs are in lowercase letters a-z.
 
  */
 
-
+// Brilliant solution by linfongi (https://leetcode.com/linfongi/)
+// https://leetcode.com/problems/longest-common-prefix/discuss/120133/Simple-JavaScript-solution
 const longestCommonPrefix = strs => {
-
+    if (!strs.length) return '';
+    for (let i = 0; i < strs[0].length; i++) {
+        for (const word of strs) {
+            if (word[i] !== strs[0][i]) {
+                return word.slice(0, i);
+            }
+        }
+    }
+    return strs[0];
 };
+
 
 module.exports = {
     solution: longestCommonPrefix,
