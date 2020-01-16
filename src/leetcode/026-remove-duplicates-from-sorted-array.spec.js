79d514bb7a7b4400e2929869ab64957c613b1048
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-15 23:23:34 -0600

    26. Remove Duplicates from Sorted Array: done

---
+++ src/leetcode/026-remove-duplicates-from-sorted-array.spec.js
@@ -0,0 +1,16 @@
+const {solution} = require('./026-remove-duplicates-from-sorted-array');
+
+describe('Remove Duplicates from Sorted Array', () => {
+    it('solution is a function', () => {
+        expect(typeof solution).toEqual('function');
+    });
+
+    it('Should be no duplicates if [1,1,2] passed in', () => {
+        expect(solution([1, 1, 2])).toBe(2);
+    });
+
+    it('Should be no duplicates if [0,0,1,1,1,2,2,3,3,4] passed in', () => {
+        expect(solution([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])).toBe(5);
+    });
+
+});
