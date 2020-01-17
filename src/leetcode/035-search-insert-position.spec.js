e5d05792de037191338bebe6e680fd9dd75aea1c
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-17 15:15:36 -0600

    35. Search Insert Position: done

--- src/leetcode/035-search-insert-position.spec.js
+++ src/leetcode/035-search-insert-position.spec.js
@@ -16,7 +16,25 @@ describe('Search Insert Position', () => {
     it('Should be 4 if [1,3,5,6], 7 passed in', () => {
         expect(solution([1, 3, 5, 6], 7)).toBe(4);
     });
+
     it('Should be 0 if [1,3,5,6], 0 passed in', () => {
         expect(solution([1, 3, 5, 6], 0)).toBe(0);
     });
+
+    it('Should be 0 if [1], 0 passed in', () => {
+        expect(solution([1], 0)).toBe(0);
+    });
+
+    it('Should be 2 if [1, 3], 4 passed in', () => {
+        expect(solution([1, 3], 4)).toBe(2);
+    });
+
+    it('Should be 0 if [1, 3, 5], 0 passed in', () => {
+        expect(solution([1, 3, 5], 0)).toBe(0);
+    });
+
+    it('Should be 6 if [1, 3, 4, 6, 7, 10], 11 passed in', () => {
+        expect(solution([1, 3, 4, 6, 7, 10], 11)).toBe(6);
+    });
+
 });
