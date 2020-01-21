914d1a46f7a4fc7ffc14b4e07c360d9053ef1375
Evgeniy Poznyak <evgeniy.poznyak@coxautoinc.com>
2020-01-21 11:44:02 -0600

    Reduce Advanced - Part 4 of Functional Programming in JavaScript https://www.youtube.com/watch?v=1DMolJ2FrNY

---
+++ src/javascript-api/reduce/001.reduce.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const fileContent = fs.readFileSync(__dirname + '/001.reduce.txt', 'utf8');
+
+const processFile = () => {
+    const result = fileContent
+        .trim()
+        .split('\n')
+        .map(line => line.split('|'))
+        .reduce((customers, line) => {
+            const inventory = {
+                name: line[1],
+                price: line[2],
+                quantity: line[3],
+            };
+            customers[line[0]] = customers[line[0]] || [];
+            customers[line[0]].push(inventory);
+            return customers;
+        }, {})
+    ;
+    // console.log(JSON.stringify(result, null, 2));
+    return result;
+};
+
+module.exports = {
+    solution: processFile,
+};
