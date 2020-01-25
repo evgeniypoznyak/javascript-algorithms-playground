64869f161dbb8995b7383bee0b8631a8f2857e77
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-01-25 10:07:39 -0600

    Exercise 1. Austentically trees

--- src/intervews/toptal/002-codility.js
+++ src/intervews/toptal/002-codility.js
@@ -1,6 +1,45 @@
 // Exercise 2
 
+const solution = A => {
+    const smaller = A => {
+        let counter = 0;
+        for (let i = 0; i < A.length; i++) {
+            if (A[i + 1] !== undefined) {
+                if (i % 2 === 0) {
+                    if (A[i] > A[i + 1]) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    };
+
+    const bigger = A => {
+        let counter = 0;
+        for (let i = 0; i < A.length; i++) {
+            if (A[i + 1] !== undefined) {
+                if (i % 1 === 0) {
+                    if (A[i] < A[i + 1]) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    };
+    let result = 0;
+    if (A.length > 1) {
+        if (A[0] < A[1]) {
+            result = smaller(A);
+        }
+        if (A[0] > A[1]) {
+            result = bigger(A);
+        }
+    }
+    return result;
+};
 
 module.exports = {
-    solution: someFuntion,
+    solution: solution,
 };
--- src/intervews/toptal/002-codility.spec.js
+++ src/intervews/toptal/002-codility.spec.js
@@ -5,8 +5,14 @@ describe('Exercise 2', () => {
         expect(typeof solution).toEqual('function');
     });
 
+    it('Should solution return 1', () => {
+        const actual = solution([5, 4, 3, 2, 6]);
+        const expected = 1;
+        expect(actual).toEqual(expected);
+    });
+
     it('Should solution return 0', () => {
-        const actual = solution();
+        const actual = solution([3, 7, 4, 5]);
         const expected = 0;
         expect(actual).toEqual(expected);
     });
