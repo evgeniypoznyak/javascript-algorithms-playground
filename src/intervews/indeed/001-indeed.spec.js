2122eca5be9ddc78955ed3196e1b76af3d260ebc
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-02-20 00:15:49 -0600

    168. 168. Excel Sheet Column Title + Indeed.

---
+++ src/intervews/indeed/001-indeed.spec.js
@@ -0,0 +1,38 @@
+const {solution} = require('./001-indeed');
+
+describe('Indeed Exercise 1', () => {
+    it('solution is a function', () => {
+        expect(typeof solution).toEqual('function');
+    });
+
+    it('Should solution return modified values', () => {
+        const data = [
+            'I',
+            'am',
+            'know',
+            'how',
+            'to',
+            'pass',
+            'indeed',
+            'test',
+            'even',
+            'if',
+            'test',
+            'unbelievable',
+            'hard',
+        ];
+        const actual = solution(data, 8);
+        const expected = [
+            'I-am',
+            'know-how',
+            'to-pass',
+            'indeed',
+            'test',
+            'even-if',
+            'test',
+            'hard',
+        ];
+        expect(actual).toStrictEqual(expected);
+    });
+
+});
